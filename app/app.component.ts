f10219662a4ee25c45f41015a8d520b3c50f3b68
Paul Everitt <pauleveritt@me.com>
2016-04-18 08:54:44 -0400

    TSLint.

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,8 +1,18 @@
 import {Component} from 'angular2/core';
 
+export class Hero {
+    id: number;
+    name: string;
+}
+
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    template: '<h1>{{title}}</h1><h2>{{hero.name}} details!</h2>'
 })
 export class AppComponent {
+    title = 'Tour of Heroes';
+    hero: Hero = {
+        id: 1,
+        name: 'Windstorm'
+    };
 }

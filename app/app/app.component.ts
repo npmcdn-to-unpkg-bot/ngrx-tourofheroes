c87fbda593c7d0d591e7fa1ee5549dac1d6967be
Paul Everitt <pauleveritt@me.com>
2016-04-18 10:52:18 -0400

    And, final component.

--- app/app/app.component.ts
+++ app/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from 'angular2/core';
 import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from 'angular2/router';
 
-import { HeroService } from './hero/hero.service';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { HeroesComponent } from './heroes/heroes.component';
-import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroService } from '../hero/hero.service';
+import { DashboardComponent } from '../dashboard/dashboard.component';
+import { HeroesComponent } from '../heroes/heroes.component';
+import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
 
 @Component({
     selector: 'my-app',
@@ -16,7 +16,7 @@ import { HeroDetailComponent } from './hero-detail/hero-detail.component';
     </nav>
     <router-outlet></router-outlet>
   `,
-    styleUrls: ['app/app.component.css'],
+    styleUrls: ['app/app/app.component.css'],
     directives: [ROUTER_DIRECTIVES],
     providers: [
         ROUTER_PROVIDERS,
